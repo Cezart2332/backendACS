ab6e07cd2dab77a1c3f415069869d17ec797ceab
Cezar <cezarturliu25@gmail.com>
2025-10-28 07:40:27 +0200

    V6

--- backend.py
+++ backend.py
@@ -1,47 +1,48 @@
-import cv2
 import os
+import time
+import cv2
 import numpy as np
 from numpy import linalg as la
 import statistics as st
-import time
+
 
 from flask import Flask
 
 
 poze = []
-nrPersoane = 40
-nrPozeTotale = 10
+nr_persoane = 40
+nr_poze_totale = 10
 A = None  
 T = None
-nrPozeAntrenare = 0
+nr_poze_antrenare = 0
 preprocessed = False
 
 norme = ["Manhattan", "Euclidean", "Infinit", "Cosinus"]
 
 
 def preprocessing(trainingNum):
-    global A, T, nrPozeAntrenare,preprocessed
-    nrPozeAntrenare = trainingNum
+    global A, T, nr_poze_antrenare, preprocessed
+    nr_poze_antrenare = trainingNum
     A = np.zeros([10304, 320])
     T = np.zeros([10304, 80])
     path = "/home/cezar/Desktop/ProiectACS/Lab2/att_faces"
     if not os.path.isdir(path):
         raise FileNotFoundError(f"Folderul '{path}' nu exista. Verifica calea.")
 
-    for i in range(1, nrPersoane + 1):
-        personPath = path + "/s" + str(i) + "//"
-        for j in range(1, nrPozeAntrenare + 1):
-            personTrainingPath = personPath + str(j) + ".pgm"
-            trainingPhoto = np.array(cv2.imread(personTrainingPath, 0)) 
-            trainingPhoto = trainingPhoto.reshape(
+    for i in range(1, nr_persoane + 1):
+        person_path = path + "/s" + str(i) + "//"
+        for j in range(1, nr_poze_antrenare + 1):
+            person_training_path = person_path + str(j) + ".pgm"
+            training_photo = np.array(cv2.imread(person_training_path, 0)) 
+            training_photo = training_photo.reshape(
                 10304,
             )
-            A[:, 8 * (i - 1) + (j - 1)] = trainingPhoto
+            A[:, 8 * (i - 1) + (j - 1)] = training_photo
 
-    for i in range(1, nrPersoane + 1):
-        personPath = path + "/s" + str(i) + "//"
-        for j in range(nrPozeAntrenare + 1, nrPozeTotale + 1):
-            personTestPath = personPath + str(j) + ".pgm"
+    for i in range(1, nr_persoane + 1):
+        person_path = path + "/s" + str(i) + "//"
+        for j in range(nr_poze_antrenare + 1, nr_poze_totale + 1):
+            personTestPath = person_path + str(j) + ".pgm"
             testPhoto = np.array(cv2.imread(personTestPath, 0))
             testPhoto = testPhoto.reshape(
                 10304,
@@ -52,8 +53,8 @@ def preprocessing(trainingNum):
     preprocessed = True
 
 
-def NN(norm, A, p):
-    if preprocessed == False:
+def nn(norm, A, p):
+    if preprocessed is False:
         raise Exception("Datele nu au fost preprocesate. Ruleaza preprocessing() mai intai.")
     z = np.zeros(len(A[0]))
     for i in range(len(A[0])):
@@ -71,8 +72,8 @@ def NN(norm, A, p):
     return pozitia
 
 
-def kNN(norm, A, p, k):
-    if preprocessed == False:   
+def k_nn(norm, A, p, k):
+    if preprocessed is False:
         raise Exception("Datele nu au fost preprocesate. Ruleaza preprocessing() mai intai.")
     z = np.zeros(len(A[0]))
     for i in range(len(A[0])):
@@ -87,25 +88,25 @@ def kNN(norm, A, p, k):
         else:
             ValueError("Norma Invalida!")
     indicii = np.argsort(z)[:k]
-    pozitii = indicii // nrPozeAntrenare
+    pozitii = indicii // nr_poze_antrenare
     pozitia = st.mode(pozitii) * 8
     return pozitia
 
 
-def statistics(nrPozeAntrenare):
-    if preprocessed == False:
+def statistics(nr_poze_antrenare):
+    if preprocessed is False:
         raise Exception("Datele nu au fost preprocesate. Ruleaza preprocessing() mai intai.")
-    nrTotalTeste = nrPersoane * (10 - nrPozeAntrenare)
-    rataRecunoastereText = "Statistici NN:\n"
-    timpInterogareText = "Statistici NN:\n"
-    print("Statistici NN:")
+    nrTotalTeste = nr_persoane * (10 - nr_poze_antrenare)
+    rataRecunoastereText = "Statistici nn:\n"
+    timpInterogareText = "Statistici nn:\n"
+    print("Statistici nn:")
     for norma in norme:
         nrRecunoasteriCorecte = 0
         timpTotalInterogare = 0
         for j in range(len(T[0])):
             t0 = time.perf_counter()
             persoanaTestata = j // 2
-            persoanaCautata = NN(norma, A, T[:, j]) // 8
+            persoanaCautata = nn(norma, A, T[:, j]) // 8
             t1 = time.perf_counter()
             timpTotalInterogare += t1 - t0
             if persoanaTestata == persoanaCautata:
@@ -117,9 +118,9 @@ def statistics(nrPozeAntrenare):
         rataRecunoastereText += f"Rata de recunoastere norma={norma}: {rr*100:.2f}%\n"
         timpInterogareText += f"Timp mediu de interogare norma={norma}: {tmi:.8f}\n"
 
-    rataRecunoastereText += "Statistici kNN:\n"
-    timpInterogareText += "Statistici kNN:\n"
-    print("Statistici kNN:")
+    rataRecunoastereText += "Statistici k_nn:\n"
+    timpInterogareText += "Statistici k_nn:\n"
+    print("Statistici k_nn:")
     for k in range(1, 9, 2):
         for norma in norme:
             nrRecunoasteriCorecte = 0
@@ -127,7 +128,7 @@ def statistics(nrPozeAntrenare):
             for j in range(len(T[0])):
                 t0 = time.perf_counter()
                 persoanaTestata = j // 2
-                persoanaCautata = kNN(norma, A, T[:, j], k) // 8
+                persoanaCautata = k_nn(norma, A, T[:, j], k) // 8
                 t1 = time.perf_counter()
                 timpTotalInterogare += t1 - t0
                 if persoanaTestata == persoanaCautata:
@@ -155,11 +156,11 @@ def hello_world():
 
 @app.route("/statistics")
 def statistics_route():
-    rataRecunoastereText, timpInterogareText = statistics(nrPozeAntrenare)
+    rataRecunoastereText, timpInterogareText = statistics(nr_poze_antrenare)
     return f"<pre>{rataRecunoastereText}\n{timpInterogareText}</pre>"
 
 
 @app.route("/preprocessing/<int:trainingNum>")
 def preprocessing_route(trainingNum):
     preprocessing(trainingNum)
-    return f"<p>Preprocessing realizat cu {trainingNum} poze de antrenare. si {nrPozeTotale - trainingNum} poze de testare.</p>"
+    return f"<p>Preprocessing realizat cu {trainingNum} poze de antrenare. si {nr_poze_totale - trainingNum} poze de testare.</p>"
